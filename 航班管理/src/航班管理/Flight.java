e64f15947099ad704fff8758a7e9cab2740ed212
zero <Aatrox@192.168.1.106>
2019-06-13 18:42:06 +0800

    6

---
+++ 航班管理/src/航班管理/Flight.java
@@ -0,0 +1,11 @@
+package ∫Ω∞‡π‹¿Ì;
+
+public class Flight implements FlightInterface{
+	private String FlightName;
+	private int Row;
+	private int RowLength:
+	private int[] fail= {-1};
+	private Passenger[] PassengerList;
+	public Flight(String FlightName,int Row,int RowLength)
+
+}

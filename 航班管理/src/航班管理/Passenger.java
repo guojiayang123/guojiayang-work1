c7990221900318118c0bb0f3f56c1deb663791a3
zero <Aatrox@192.168.1.106>
2019-06-13 17:52:15 +0800

    2

---
+++ 航班管理/src/航班管理/Passenger.java
@@ -0,0 +1,19 @@
+package ∫Ω∞‡π‹¿Ì;
+
+public class Passenger implements PassengerInterface
+{
+	private String Names;
+	private int BookingNumeber;
+	private int Rrows;
+	private int SeatPosition;
+	
+	public Passenger(String Names,int BookingNumeber,int Rrows,int SeatPosition) {
+		this.Names=Names;
+		this.BookingNumeber=BookingNumeber;
+		this.Rrows=Rrows;
+		this.SeatPosition=SeatPosition;
+	}
+	
+	
+
+}

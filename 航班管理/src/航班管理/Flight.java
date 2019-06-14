e87e377e7189286ad78032024796e4f4f71048f3
zero <Aatrox@192.168.1.106>
2019-06-14 13:15:28 +0800

    7

--- 航班管理/src/航班管理/Flight.java
+++ 航班管理/src/航班管理/Flight.java
@@ -3,9 +3,21 @@ package ∫Ω∞‡π‹¿Ì;
 public class Flight implements FlightInterface{
 	private String FlightName;
 	private int Row;
-	private int RowLength:
+	private int RowLength;
 	private int[] fail= {-1};
 	private Passenger[] PassengerList;
-	public Flight(String FlightName,int Row,int RowLength)
+	public Flight(String FlightName,int Rows,int RowLength)throws Exception{
+		if(FlightName==null||FlightName.trim().length()==0||Rows<=0||RowLength<=0)
+			throw new Exception("Error");
+		else {
+			this.FlightName=FlightName;
+			this.Row=Rows;
+			this.RowLength=RowLength;
+			this.PassengerList=new Passenger[Row*RowLength];
+			for(int i=0;i<Row*RowLength;i++)
+				PassengerList[i]=null;
+			
+		}
+	}
 
 }
